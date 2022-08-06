01bdf184bfa6111bac340ce044125ba8b268c9d8
TebLeg <TebLeg@users.noreply.github.com>
2022-08-06 13:04:55 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express'); // Include ExpressJS
+const app = express(); // Create an ExpressJS app
+const bodyParser = require('body-parser'); // Middleware that parses user input and makes it available through the req.body property.
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Route to Calculator
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/static/calculator.html');
+});
+
+// Route to Login Page
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/static/calculator.html');
+});
+
+app.post('/calculate', (req, res) => {
+  // Insert Login Code Here
+  let username = req.body.username;
+  let password = req.body.password;
+  res.send(`Username: ${username} Password: ${password}`);
+});
+
+const port = process.env.PORT || 3000; // Port we will listen on
+
+// Function to listen on the port
+app.listen(port, () => console.log(`This app is listening on port ${port}`));
